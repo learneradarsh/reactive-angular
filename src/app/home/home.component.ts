26f7a42068410c24381b821c9c20017ad12396ce
Adarsh Learner <learner.adarsh@gmail.com>
2022-09-11 14:28:55 +0530

    Refactored home component for reactive approach

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,9 +2,9 @@ import {Component, OnInit} from '@angular/core';
 import {Course, sortCoursesBySeqNo} from '../model/course';
 import {interval, noop, Observable, of, throwError, timer} from 'rxjs';
 import {catchError, delay, delayWhen, filter, finalize, map, retryWhen, shareReplay, tap} from 'rxjs/operators';
-import {HttpClient} from '@angular/common/http';
 import {MatDialog, MatDialogConfig} from '@angular/material/dialog';
 import {CourseDialogComponent} from '../course-dialog/course-dialog.component';
+import { ApiService } from '../services/api.service';
 
 
 @Component({
@@ -14,28 +14,26 @@ import {CourseDialogComponent} from '../course-dialog/course-dialog.component';
 })
 export class HomeComponent implements OnInit {
 
-  beginnerCourses: Course[];
+  beginnerCourses$: Observable<Course[]>;
 
-  advancedCourses: Course[];
+  advancedCourses$: Observable<Course[]>;
 
 
-  constructor(private http: HttpClient, private dialog: MatDialog) {
+  constructor(private apiService: ApiService, private dialog: MatDialog) {
 
   }
 
   ngOnInit() {
 
-    this.http.get('/api/courses')
-      .subscribe(
-        res => {
+    const couses$ = this.apiService.loadCourses$();
 
-          const courses: Course[] = res["payload"].sort(sortCoursesBySeqNo);
+    this.beginnerCourses$ = couses$.pipe(
+      map(courses => courses.filter(course => course.category === "BEGINNER"))
+    );
 
-          this.beginnerCourses = courses.filter(course => course.category == "BEGINNER");
-
-          this.advancedCourses = courses.filter(course => course.category == "ADVANCED");
-
-        });
+    this.advancedCourses$ = couses$.pipe(
+      map(courses => courses.filter(course => course.category === "ADVANCED"))
+    );
 
   }
 
